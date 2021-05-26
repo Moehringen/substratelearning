019348b4683503b571b7b3d24cc73f36677f17b6
“Moehringen <haroldstuttgart@gmail.com>
2021-05-26 22:55:27 +0800

    finish sum

--- Course4/sum/src/main.rs
+++ Course4/sum/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let a = [1,2,3,4,5,6,7,8,9,10];
+    let result = sum(&a);
+    println!("the sum of the list is: {:?}", result);
+    let b = [4294967295,1];
+    let result = sum(&b);
+    println!("the sum of the list is(overflow): {:?}", result);
+}
+
+
+
+fn sum(a: &[u32]) -> Option<u32> {
+
+    let mut sum: Option<u32> = Some(0);
+    for &i in a
+    {
+        sum = sum.unwrap().checked_add(i);
+    }
+    sum
 }
